468399a8158e0f5c8cb7bc61f86c684616c3c62f
kimbeomjun <gimb52043@gmail.com>
2024-07-24 14:46:48 +0900

    친구추가 페이지

--- src/views/follow/addFollow.vue
+++ src/views/follow/addFollow.vue
@@ -2,14 +2,48 @@
     <div class="container">
         <GoBack />
         <div class="add-friend">
-            <h3>아이디로 보호자/대상자 추가하기</h3>
-            <input type="text" id="userId" name="userId" placeholder="추가할 보호자/대상자 아이디 입력">
-            <button class="add-fri-btn">확인</button>
+            <h3>아이디로 친구 찾기</h3>
+            <input type="text" id="userId" name="userId" placeholder="추가할 유저의 아이디를 입력" v-model="findIdChk">
+            <button class="add-fri-btn" @click="findIdAdd">확인</button>
+        </div>
+        <div v-if="followIdBox" class="profile-box">
+            <div v-if="foundIdUser">
+                <div class="user-img-box" v-if="foundIdUser.imgUrl">
+                    <div class="user-profile" :style="{ backgroundImage: `url(${foundIdUser.imgUrl})` }"></div>
+                </div>
+                <div v-else class="user-img-box">
+                    <div class="user-not-profile"></div>
+                </div>
+                <p class="username">{{ foundIdUser.username }}</p>
+                <button class="req-follow" @click="addRequest(foundIdUser.id)">친구 신청하기</button>
+                <!-- <button class="req-follow" @click="addRequest(foundIdUser.id)" @click="cancelRequest">신청 취소</button>
+                <button class="req-follow" @click="addRequest(foundIdUser.id)" >친구 신청하기</button> -->
+            </div>
+            <div v-else>
+                <p>해당 유저가 없습니다</p>
+            </div>
         </div>
         <div class="add-friend">
-            <h3>연락처로 보호자/대상자 추가하기</h3>
-            <input type="text" id="usePhone" name="usePhone" placeholder="추가할 보호자/대상자 전화번호 입력">
-            <button class="add-fri-btn">확인</button>
+            <h3>연락처로 친구 찾기</h3>
+            <input type="text" id="usePhone" name="usePhone" placeholder="'-'를 제외하고 입력" v-model="findPhoneChk">
+            <button class="add-fri-btn" @click="findPhoneAdd">확인</button>
+        </div>
+        <div v-if="followPhoneBox" class="profile-box">
+            <div v-if="foundPhoneUser">
+                <div class="user-img-box" v-if="foundPhoneUser.imgUrl">
+                    <div class="user-profile" :style="{ backgroundImage: `url(${foundPhoneUser.imgUrl})` }"></div>
+                </div>
+                <div v-else class="user-img-box">
+                    <div class="user-not-profile"></div>
+                </div>
+                <p class="username">{{ foundPhoneUser.username }}</p>
+                <button class="req-follow" @click="addRequest(foundPhoneUser.id)">친구 신청하기</button>
+                <!-- <button class="req-follow" @click="addRequest(foundIdUser.id)" @click="cancelRequest">신청 취소</button>
+                <button class="req-follow" @click="addRequest(foundIdUser.id)" >친구 신청하기</button> -->
+            </div>
+            <div v-else>
+                <p>해당 유저가 없습니다</p>
+            </div>
         </div>
     </div>
 </template>
@@ -21,7 +55,84 @@ export default {
     name: "addFollow",
     components: {
         GoBack,
-    } 
+    },
+    data() {
+        return {
+            userList: [
+                {
+                    id: 1,
+                    username: "김범준",
+                    imgUrl: '../../../public/img/accessories-8826708_640.jpg',
+                    phone: "010-3395-2934",
+                    userId: "gcon1945",
+                    gender: "남",
+                    birth: "1995.12.05"
+                },
+                {
+                    id: 2,
+                    username: "김개똥",
+                    imgUrl: '',
+                    phone: "010-1234-4567",
+                    userId: "IamID",
+                    gender: "여",
+                    birth: "1997.12.01"
+                },
+            ],
+            findIdChk: "",
+            findPhoneChk: "",
+            followIdBox: false,
+            followPhoneBox: false,
+            foundIdUser: null,
+            foundPhoneUser: null,
+        }
+    },
+    methods: {
+        findIdAdd() {
+            const user = this.userList.find((user) => user.userId === this.findIdChk);
+            if (user) {
+                this.foundIdUser = user;
+                this.followIdBox = true;
+            } else {
+                this.foundIdUser = null;
+                this.followIdBox = true;
+            }
+        },
+        findPhoneAdd() {
+            const formattedPhone = this.formatPhone(this.findPhoneChk);
+            console.log("formattedPhone", formattedPhone);
+            const user = this.userList.find((user) => user.phone === formattedPhone);
+            console.log("user", user);
+            if (user) {
+                this.foundPhoneUser = user;
+                this.followPhoneBox = true;
+            } else {
+                this.foundPhoneUser = null;
+                this.followPhoneBox = true;
+            }
+        },
+        formatPhone(phone) {
+            if (phone.length === 11) {
+                return phone.replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3');
+            } else if (phone.length === 10) {
+                return phone.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+            }
+            return phone;
+        },
+        addRequest(userId) {
+            try {
+                // 신청 로직 작성
+            } catch (error) {
+                console.log("친구추가 요청 실패", error);
+            }
+        },
+        cancelRequest() {
+            try {
+                // 신청취소 로직 작성
+            } catch (error) {
+                console.log("친구추가 요청 실패", error);
+            }
+        }
+    }
 }
 </script>
 
@@ -55,5 +166,47 @@ export default {
     color: #fff;
     font-size: var(--font-n-size);
     transform: translateY(3px);
+}
+
+.profile-box {
+    text-align: center;
+    margin-bottom: 30px;
+    background: #ebebeb;
+    width: 100%;
+    width: 100%;
+    padding: 20px 0;
+}
+
+.user-img-box {
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+    overflow: hidden;
+    background: var(--input-border-color);
+    margin: 0 auto 5px;
+}
+
+.user-img-box .user-profile {
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-position: center;
+}
+
+.user-not-profile {
+    width: 100%;
+    height: 100%;
+    background: url("../../../public/img/empty_person.png") no-repeat center/cover;
+}
+
+.req-follow {
+    background: var(--main-color);
+    border: none;
+    border-radius: 5px;
+    padding: 5px 10px;
+    margin-top: 5px;
+    color: #fff;
+    letter-spacing: 1px;
+    font-size: var(--font-n-sec-size);
 }
 </style>
