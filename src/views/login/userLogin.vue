d960cf05bc56ee4f174c552d9a3c0b92eb965b46
kimbeomjun <gimb52043@gmail.com>
2024-09-05 10:01:48 +0900

    로그인

--- src/views/login/userLogin.vue
+++ src/views/login/userLogin.vue
@@ -1,44 +1,64 @@
 <template>
   <div class="container margin-bottom-zero">
-    <img src="@/assets/img/app_logo_02.png" alt="G-CON Logo" class="logo">
-    <!-- <p class="login-text">아이디, 비밀번호를 입력 후<br><span>로그인 버튼</span>을 눌러주세요</p> -->
-
-    <form class="login-form" @submit.prevent="login">
-      <div class="input-group">
-        <!-- <label for="username">아이디</label> -->
-        <input type="text" id="username" name="username" v-model="userId" required placeholder="아이디">
-      </div>
-      <div class="input-group">
-        <!-- <label for="password">비밀번호</label> -->
-        <input type="password" id="password" name="password" v-model="password" required placeholder="비밀번호">
-      </div>
-      <button type="submit" class="login-button">로그인</button>
-    </form>
-    <div class="sns-login">
-      <div class="sns-login-box" @click="redirectToNaver">
-        <div class="sns-login-naver">
-          <img src="@/assets/img/naver_login.png" alt="">
+    <div class="position-center">
+      <img src="@/assets/img/app_logo_02.png" alt="G-CON Logo" class="logo">
+      <form class="login-form" @submit.prevent="login">
+        <div class="input-group">
+          <input type="text" id="username" name="username" v-model="userId" required placeholder="아이디">
         </div>
+        <div class="input-group">
+          <input type="password" id="password" name="password" v-model="password" required placeholder="비밀번호">
+        </div>
+        <button type="submit" class="login-button">로그인</button>
+      </form>
+      <!-- <div class="sns-login">
+        <div class="sns-login-box" @click="redirectToNaver">
+          <div class="sns-login-naver">
+            <img src="@/assets/img/naver_login.png" alt="">
+          </div>
+        </div>
+        <div class="sns-login-box" @click="redirectToKakao">
+          <div class="sns-login-kakao">
+            <img src="@/assets/img/kakao_login.png" alt="">
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 20 19">
+              <path fill="#181600"
+                d="M10 .111C4.478.111 0 3.59 0 7.878 0 10.545 1.733 12.9 4.367 14.3l-1.111 4.078c-.1.356.31.644.622.433l4.866-3.222c.412.045.823.067 1.256.067 5.522 0 10-3.478 10-7.767 0-4.3-4.478-7.778-10-7.778z" />
+            </svg>
+            <div class="hidden-box"></div>
+          </div>
+        </div>
+      </div> -->
+      <div class="links">
+        <RouterLink to="/user/userIdFind">
+          <small>아이디 찾기</small>
+        </RouterLink> |
+        <RouterLink to="/user/userIdFind">
+          <small>비밀번호 찾기</small>
+        </RouterLink>
       </div>
-      <div class="sns-login-box" @click="redirectToKakao">
-        <div class="sns-login-kakao">
-          <img src="@/assets/img/kakao_login.png" alt="">
-          <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 20 19">
-            <path fill="#181600"
-              d="M10 .111C4.478.111 0 3.59 0 7.878 0 10.545 1.733 12.9 4.367 14.3l-1.111 4.078c-.1.356.31.644.622.433l4.866-3.222c.412.045.823.067 1.256.067 5.522 0 10-3.478 10-7.767 0-4.3-4.478-7.778-10-7.778z" />
-          </svg>
-          <div class="hidden-box"></div>
+  
+      <div class="sns-login">
+        <div class="sns-title-box">
+          <div class="sns-border-flex">
+            <div class="sns-border"></div>
+            <small>SNS 계정으로 로그인</small>
+            <div class="sns-border"></div>
+          </div>
+        </div>
+        <div class="sns-icon-wrap">
+          <div class="sns-icon-box" @click="redirectToKakao">
+            <img src="@/assets/img/free-icon-kakao-talk-4494622.png" alt="카카오 로그인">
+          </div>
+          <div class="sns-icon-box" @click="redirectToNaver">
+            <img src="@/assets/img/pngwing.com.png" alt="네이버 로그인">
+          </div>
         </div>
       </div>
     </div>
-    <div class="links">
-      <RouterLink to="/user/userIdFind">아이디 찾기</RouterLink> | <RouterLink to="/user/userIdFind">비밀번호 찾기</RouterLink>
-    </div>
-
-    <div class="moveSignup">
-      <span>스마트 신발 회원이 아니신가요?</span>
+    <div class="move-signup">
+      <small>스마트 신발 회원이 아니신가요?</small>
       <RouterLink to="/signup/signupView">
-        <span style="color: var(--main-color);">회원가입</span>
+        <small style="color: var(--main-color);">회원가입</small>
       </RouterLink>
     </div>
   </div>
@@ -96,23 +116,23 @@ export default {
 </script>
 
 <style scoped>
-/* .login-text {
-  word-break: keep-all;
-  text-align: center;
+.container {
+  padding: 0;
 }
 
-.login-text span {
-  color: var(--main-color);
-  font-weight: var(--font-b-weight);
-} */
-
-.container {
-  padding: 40px 20px;
+.position-center {
+  padding: 40px 30px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
 }
 
 .login-form {
   width: 100%;
-  max-width: 300px;
   display: flex;
   flex-direction: column;
   margin-top: 20px;
@@ -141,35 +161,83 @@ export default {
   margin-top: 10px;
   border: none;
   border-radius: var(--border-radius);
-  font-size: 19px;
+  font-size: var(--font-n-sec-size);
   color: #fff;
   background-color: var(--main-color);
-  font-weight: var(--font-t-weight);
 }
-
-/* .login-button:last-child {
-  width: 100%;
-  height: 56px;
-  position: relative;
-  text-align: center;
-  line-height: 53px;
-}
-
-.login-button:last-child a {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-} */
 
 .links {
   margin-top: 20px;
-  font-size: var(--font-n-sec-size);
+  font-weight: var(--font-t-weight);
   color: var(--light-font-color);
 }
 
 .sns-login {
+  width: 100%;
+  margin-top: 40px;
+}
+
+.sns-title-box {
+  color: var(--light-font-color);
+  font-weight: var(--font-t-weight);
+}
+
+.sns-border-flex small {
+  width: 100%;
+  margin: 0 5px;
+  text-align: center;
+  white-space: nowrap;
+}
+
+.sns-border-flex {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.sns-border {
+  width: 100%;
+  height: 1px;
+  background: var(--input-border-color);
+}
+
+.sns-icon-wrap {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  padding: 20px;
+}
+
+.sns-icon-box {
+  width: 50px;
+  height: 50px;
+}
+
+/* .sns-title-box:before {
+  content: '';
+  position: absolute;
+  top: 50%;
+  left: -105%;
+  transform: translateY(-50%);
+  width: 27.320843091334897vw;
+  height: 1px;
+  background: var(--input-border-color);
+}
+
+.sns-title-box:after {
+  content: '';
+  position: absolute;
+  top: 50%;
+  right: -105%;
+  transform: translateY(-50%);
+  width: 27.320843091334897vw;
+  height: 1px;
+  background: var(--input-border-color);
+} */
+
+
+/* .sns-login {
   margin-top: 20px;
 }
 
@@ -218,12 +286,16 @@ export default {
   left: 0;
   transform: translateY(-50%);
   z-index: 1;
+} */
+
+.move-signup {
+  position: absolute;
+  bottom: 50px;
+  font-weight: var(--font-t-weight);
 }
 
-.moveSignup {
-  display: flex;
-  align-items: center;
-  margin-top: 20px;
-  gap: 10px;
+.move-signup small {
+  margin-right: 5px;
+  color: var(--light-font-color);
 }
 </style>
